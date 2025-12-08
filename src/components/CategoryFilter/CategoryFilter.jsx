89b468da06c9db5106ba33ee91180be181a341b5
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-12-08 11:54:26 +0330

    feat: update cn

--- src/components/CategoryFilter/CategoryFilter.jsx
+++ src/components/CategoryFilter/CategoryFilter.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { category } from "../../services/category";
-import { cn } from "../../utils/utils";
+import { cn } from "../../lib/utils";
 import { useState } from "react";
 import {Link} from "react-router"
 
@@ -23,7 +23,7 @@ const Category = () => {
   return (
     <div className="flex gap-2.5">
       {cat.map(item => <Link  to="/result" key={item.id} className="bg-slate-800 px-3 py-1.5 rounded-xl">{item.title}</Link> )}
-      <div className={cn(["bg-slate-800 px-3 py-1.5 rounded-xl" && toggle ? "hidden" : "block"])}>
+      <div className={cn("bg-slate-800 px-3 py-1.5 rounded-xl", toggle ? "hidden" : "block")}>
         <button className="bg-slate-800 px-3 py-1.5 rounded-xl" onClick={bOnClick}>LoadMore <span className="ml-2">❯</span></button>
       </div>
     </div>
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,6 @@
-// 
-export const cn = (items) =>{
-  return items.filter(Boolean).join(" ")
+import { cx } from "class-variance-authority"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs) {
+  return twMerge(cx(inputs))
 }
